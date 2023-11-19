20400c0108070c790d61f1b1a2d5cbbab470ec71
Roman Blinov <romanblinov2013@yandex.ru>
2023-11-19 03:04:47 +0300

    Add settings package

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -13,4 +13,5 @@ func main() {
 
 	// gui.Start()
 	tg.Start()
+	// settings.Couts()
 }
---
+++ internal/Settings/cout.go
@@ -0,0 +1,59 @@
+// Пакет для настройки и второстепенных задач
+
+package settings
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+
+	minfocolor "github.com/RB-PRO/infocolor/pkg/go-infocolor/m-infocolor"
+)
+
+func Couts() {
+	Folder := "infocolor/"
+	files, _ := FileList(Folder)
+	var cout int
+	for _, file := range files {
+		data, _ := LoadFile(Folder, file)
+		cout += len(data)
+		fmt.Println(file, ",", len(data))
+	}
+	fmt.Println(cout)
+}
+func FileList(Folder string) ([]string, error) {
+	files, err := os.ReadDir(Folder)
+	if err != nil {
+		return nil, fmt.Errorf("os.ReadDir(%s): %v", Folder, err)
+	}
+	FileNames := make([]string, 0, 55)
+	for _, f := range files {
+		FileName := strings.ReplaceAll(f.Name(), ".json", "")
+		FileNames = append(FileNames, FileName)
+	}
+	return FileNames, nil
+}
+
+// Загрузить данные из файла
+func LoadFile(Folder, filename string) (data []minfocolor.Formulass, ErrorFile error) {
+	// Открыть файл
+	jsonFile, ErrorFile := os.Open(Folder + filename + ".json")
+	if ErrorFile != nil {
+		return data, ErrorFile
+	}
+	defer jsonFile.Close()
+
+	// Прочитать файл и получить массив byte
+	jsonData, ErrorFile := io.ReadAll(jsonFile)
+	if ErrorFile != nil {
+		return data, ErrorFile
+	}
+
+	// Распарсить массив byte в структуру
+	if ErrorFIle := json.Unmarshal(jsonData, &data); ErrorFIle != nil {
+		return data, ErrorFile
+	}
+	return data, ErrorFile
+}
